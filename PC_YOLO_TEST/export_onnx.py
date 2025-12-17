3a6b9b03dc12efb788c6624b090bc54b4a581824
zwd <1712332665@qq.com>
2025-12-18 01:38:56 +0800

    model changed

--- PC_YOLO_TEST/export_onnx.py
+++ PC_YOLO_TEST/export_onnx.py
@@ -1,6 +1,6 @@
-#将官方 YOLO 模型导出为具有动态批大小和图像大小的 ONNX。
+#将官方 YOLO 模型导出为具有动态批大小和图像大小的 ONNX
 
 from ultralytics import YOLO
 
-model = YOLO("yolo11n.pt")
+model = YOLO("yolov8s.pt")
 model.export(format="onnx", dynamic=True)
--- PC_YOLO_TEST/get_item_code.py
+++ PC_YOLO_TEST/get_item_code.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
 
-model = YOLO('yolo11n.pt')
+model = YOLO('yolov8s.pt')
 
 print(model.names)
